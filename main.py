294e9e8fc9834b2aa2811745b1b96c4dbda59e1f
asbrubx12222k0777 <144273118+asbrubx12222k0777@users.noreply.github.com>
2023-09-17 11:25:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# Factorial of a number using recursion
-def recur_factorial(n):
-  if n == 1:
-    return n
-  else:
-    return n * recur_factorial(n - 1)
-
-
-num = 7
-# check if the number is negative
-if num < 0:
-  print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-  print("The factorial of 0 is 1")
-else:
-  print("The factorial of", num, "is", recur_factorial(num))
+# A Python program to demonstrate inheritance 
+  
+# Base or Super class. Note object in bracket.
+# (Generally, object is made ancestor of all classes)
+# In Python 3.x "class Person" is 
+# equivalent to "class Person(object)"
+class Person(object):
+      
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+  
+    # To get name
+    def getName(self):
+        return self.name
+  
+    # To check if this person is employee
+    def isEmployee(self):
+        return False
+  
+  
+# Inherited or Sub class (Note Person in bracket)
+class Employee(Person):
+  
+    # Here we return true
+    def isEmployee(self):
+        return True
+  
+# Driver code
+emp = Person("Geek1")  # An Object of Person
+print(emp.getName(), emp.isEmployee())
+  
+emp = Employee("Geek2") # An Object of Employee
+print(emp.getName(), emp.isEmployee())
