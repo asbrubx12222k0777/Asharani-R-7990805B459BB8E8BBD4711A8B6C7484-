71f3ece115721717a3fb9c3a8b4b8720c60b6d23
asbrubx12222k0777 <144273118+asbrubx12222k0777@users.noreply.github.com>
2023-09-17 12:48:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-# A Python program to demonstrate inheritance 
-  
-# Base or Super class. Note object in bracket.
-# (Generally, object is made ancestor of all classes)
-# In Python 3.x "class Person" is 
-# equivalent to "class Person(object)"
-class Person(object):
-      
-    # Constructor
-    def __init__(self, name):
-        self.name = name
-  
-    # To get name
-    def getName(self):
-        return self.name
-  
-    # To check if this person is employee
-    def isEmployee(self):
-        return False
-  
-  
-# Inherited or Sub class (Note Person in bracket)
-class Employee(Person):
-  
-    # Here we return true
-    def isEmployee(self):
-        return True
-  
-# Driver code
-emp = Person("Geek1")  # An Object of Person
-print(emp.getName(), emp.isEmployee())
-  
-emp = Employee("Geek2") # An Object of Employee
-print(emp.getName(), emp.isEmployee())
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
